244b5c4683c39d44c56278c1f2844588d206706f
Izzatul Masruroh <izza@zero-one-group.com>
2024-06-28 09:17:13 +0700

    feat/4-add-cart-model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:state_management/app_router.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 // import 'package:state_management/models/cart.dart';
 // import 'package:state_management/models/catalog.dart';
 
@@ -13,25 +13,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: const [],
-      child: MaterialApp.router(
-        title: 'State Management - Provider',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorSchemeSeed: Colors.blue,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.blue,
-            foregroundColor: Colors.white,
-            titleTextStyle: TextStyle(
-              fontWeight: FontWeight.w700,
-              color: Colors.white,
-              fontSize: 24,
-            ),
+    return MaterialApp.router(
+      title: 'State Management - Provider',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+            fontSize: 24,
           ),
         ),
-        routerConfig: router,
       ),
+      routerConfig: router,
     );
   }
 }
